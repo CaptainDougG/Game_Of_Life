c48e02e472dcbac69d421a7fd4ffc8111349048f
904238003 <904238003@L829WD150099.edu.cbe.ab.ca>
2021-05-25 15:03:07 -0600

    Got some help think I can do this now

--- GameOfLife/src/GameOfLife.java
+++ GameOfLife/src/GameOfLife.java
@@ -3,10 +3,12 @@ public class GameOfLife {
 	
 	int rows;
 	int columns;
+	boolean [][] game = null;
 	
 	public GameOfLife(int rows, int columns) {
 		this.rows = rows;
 		this.columns = columns;
+		game = new boolean[rows][columns];
 	}
 	
 	public int getRows() {
@@ -22,17 +24,23 @@ public class GameOfLife {
 		if (row > rows || row <= 0 || column > columns || column <= 0) {
 			isAlive = false;
 		}
+		else {
+			isAlive = game[row][column];
+		}
 		
 		return isAlive;
 		
 	}
 	
-	public static void setAlive(int row, int columns, boolean life) {
-		
+	public static void setAlive(int row, int column, boolean life) {
+
 	}
 	
-	public static int getNeighbourCount(int a, int b) {
-		
+	public int getNeighbourCount(int row, int column) {
+		int neighbours = 0;
+		if (isAlive(row-1, column-1)) {
+			 neighbours += 1;
+		}
 	}
 	
 	public static void calculateNextGeneration() {
